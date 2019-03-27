95a64b6d8507c6a07b60f7142e68c904e8ea10ef
Jared Harbison <jared.harbison@gmail.com>
2019-03-27 15:21:05 -0400

    Done.

---
+++ app/models/release_date_validator.rb
@@ -0,0 +1,14 @@
+
+require 'date'
+
+ class ReleaseDateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if record.released == true
+      if value.blank?
+        record.errors.add attribute, (options[:message] || "must give a release year if the song has been released")
+      elsif value > Date::today.year
+        record.errors.add attribute, (options[:message] || "release year must not be in the future")
+      end
+    end
+  end
+end
