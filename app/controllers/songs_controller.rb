1ad059ffd319caef9bf00d1a255c3d61646711a2
gbs4ever <gbs4ever2@gmail.com>
2019-03-26 22:11:38 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,8 +2,13 @@ class SongsController < ApplicationController
 def index
   @songs = Song.all
 end
+def show 
+  @song= Song.find(params[:id])
+end
+
 def new 
-  @song = Song.new(song_params)
+  @song = Song.new
+
 end
 def create
  @song = Song.new(song_params)
@@ -14,30 +19,31 @@ def create
  end
 end
 
-def show 
-  @song= Song.find(params[:id])
-end
+
 
 def edit
    @song= Song.find(params[:id])
 end
 
-def update
- @song.update(song_params)
-    if @song.valid?
-     @song.save
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
      redirect_to song_path(@song)
-    else
-    render :edit
+      else
+      render :edit
     end
+ end
+
+
+
+  def song_params
+    params.require(:song).permit(:title, :released ,:release_year,:artist_name,:genre)
   end
-
-
-
-def song_params
-    params.permit(:title, :released ,:release_year,:artist_name,:genre)
-end
-
+  def destroy
+     @song= Song.find(params[:id])
+     @song.destroy
+     redirect_to songs_path
+  end
 
 end
 
